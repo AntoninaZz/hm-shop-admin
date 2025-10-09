01bab796c3afb0ea6262d9aa1dd553c4cdef80b7
AntoninaZz <zdanton02@gmail.com>
2025-10-09 14:22:04 +0300

    allow e-commerce store access for getting amount for liqpay

--- app/api/orders/customers/[customerId]/route.ts
+++ app/api/orders/customers/[customerId]/route.ts
@@ -7,7 +7,14 @@ export const GET = async (req: NextRequest, { params }: { params: { customerId: 
     try {
         await connectToDB();
         const orders = await Order.find({ customerClerkId: params.customerId }).populate({ path: "products.product", model: Product });
-        return NextResponse.json(orders, { status: 200 })
+        return NextResponse.json(orders, {
+            status: 200,
+            headers: {
+                "Access-Control-Allow-Origin": `${process.env.ECOMMERCE_STORE_URL}`,
+                "Access-Control-Allow-Methods": "GET",
+                "Access-Control-Allow-Headers": "Content-Type",
+            },
+        })
     } catch (error) {
         console.log("[customerId_GET]", error);
         return new NextResponse("Internal Server Error", { status: 500 });
