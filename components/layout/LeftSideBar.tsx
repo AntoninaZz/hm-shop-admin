eb3489670e150309133da4c1d2785d427903be8a
AntoninaZz <zdanton02@gmail.com>
2025-10-09 15:24:56 +0300

    adapt sidebar for smaller laptops

--- components/layout/LeftSideBar.tsx
+++ components/layout/LeftSideBar.tsx
@@ -10,12 +10,12 @@ import { navLinks } from '@/lib/constants';
 const LeftSideBar = () => {
     const path = usePathname();
     return (
-        <div className='h-screen left-0 top-0 sticky py-10 flex flex-col justify-between gap-8 bg-[var(--color-milk)] text-medium shadow-xl max-lg:hidden'>
-            <div className='flex flex-col gap-8'>
+        <div className='h-screen left-0 top-0 sticky py-7 flex flex-col justify-between gap-8 bg-[var(--color-milk)] text-medium shadow-xl max-lg:hidden'>
+            <div className='flex flex-col gap-5'>
                 <div className='flex justify-center'>
                     <Image src="/logo.jpg" alt="hm-shop" width={75} height={75} />
                 </div>
-                <div className='flex flex-col gap-4'>
+                <div className='flex flex-col gap-2'>
                     {navLinks.map((link, i) => (
                         <Link key={i} href={link.url} className={`flex gap-4 px-10 py-4 hover:text-[var(--color-muted-green)] transition-all ease-in-out duration-200 ${path === link.url ? 'border-r-2' : ''}`}>
                             {link.icon}
