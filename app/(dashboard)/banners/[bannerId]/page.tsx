655f58e995f99c8897ac448e1dc0d732d555f4a5
AntoninaZz <zdanton02@gmail.com>
2025-10-24 13:37:10 +0300

    fixing vercel deployment error for bannerId & categoryId pages

--- app/(dashboard)/banners/[bannerId]/page.tsx
+++ app/(dashboard)/banners/[bannerId]/page.tsx
@@ -1,6 +1,35 @@
-import BannerDetailsPage from "@/components/banners/BannerDetailsPage";
+"use client";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Loader from "@/components/custom ui/Loader";
+import BannerForm from "@/components/banners/BannerForm";
 
-export default async function Page({ params }: { params: Promise<{ bannerId: string }> }) {
-  const { bannerId } = await params;
-  return <BannerDetailsPage params={{ bannerId }} />;
+const BannerDetailsPage = () => {
+  const params = useParams();
+  const bannerId = params?.productId as string;
+  const [loading, setLoading] = useState(true);
+  const [bannerDetails, setBannerDetails] = useState<BannerType | null>(null);
+
+  const getBannerDetails = async () => {
+    try {
+      const res = await fetch(`/api/banners/${bannerId}`, {
+        method: "GET",
+      });
+      const data = await res.json();
+      setBannerDetails(data);
+      setLoading(false);
+    } catch (error) {
+      console.log("[bannerId_GET]", error);
+    }
+  }
+
+  useEffect(() => {
+    getBannerDetails();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (loading ? <Loader /> :
+    <BannerForm initialData={bannerDetails} />
+  )
 }
+
+export default BannerDetailsPage;
--- app/(dashboard)/categories/[categoryId]/page.tsx
+++ app/(dashboard)/categories/[categoryId]/page.tsx
@@ -1,6 +1,35 @@
-import CategoryDetailsPage from "@/components/categories/CategoryDetailsPage";
+"use client";
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
+import Loader from "@/components/custom ui/Loader";
+import CategoryForm from "@/components/categories/CategoryForm";
 
-export default async function Page({ params }: { params: Promise<{ categoryId: string }> }) {
-  const { categoryId } = await params;
-  return <CategoryDetailsPage params={{ categoryId }} />;
+const CategoryDetailsPage = () => {
+  const params = useParams();
+  const categoryId = params?.productId as string;
+  const [loading, setLoading] = useState(true);
+  const [categoryDetails, setCategoryDetails] = useState<CategoryType | null>(null);
+
+  const getCategoryDetails = async () => {
+    try {
+      const res = await fetch(`/api/categories/${categoryId}`, {
+        method: "GET",
+      });
+      const data = await res.json();
+      setCategoryDetails(data);
+      setLoading(false);
+    } catch (error) {
+      console.log("[categoryId_GET]", error);
+    }
+  }
+
+  useEffect(() => {
+    getCategoryDetails();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (loading ? <Loader /> :
+    <CategoryForm initialData={categoryDetails} />
+  )
 }
+
+export default CategoryDetailsPage;
