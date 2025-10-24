71aab2bdb57ef652d12416b23e88877a5b859db6
AntoninaZz <zdanton02@gmail.com>
2025-10-24 13:45:55 +0300

    fixing vercel deployment error for bannerId api route

--- app/api/banners/[bannerId]/route.ts
+++ app/api/banners/[bannerId]/route.ts
@@ -3,10 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 import Banner from "@/lib/models/Banner";
 import { connectToDB } from "@/lib/mongoDB";
 
-export const GET = async (req: NextRequest, { params }: { params: { bannerId: string } }) => {
+export const GET = async (req: NextRequest, context: { params: { bannerId: string } }) => {
     try {
+        const { bannerId } = context.params;
         await connectToDB();
-        const banner = await Banner.findById(params.bannerId);
+        const banner = await Banner.findById(bannerId);
         if (!banner) {
             return new NextResponse(JSON.stringify({ message: "Banner not found" }), { status: 404 });
         }
@@ -17,14 +18,15 @@ export const GET = async (req: NextRequest, { params }: { params: { bannerId: st
     }
 }
 
-export const POST = async (req: NextRequest, { params }: { params: { bannerId: string } }) => {
+export const POST = async (req: NextRequest, context: { params: { bannerId: string } }) => {
     try {
         const { userId } = await auth();
         if (!userId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+        const { bannerId } = context.params;
         await connectToDB();
-        let banner = await Banner.findById(params.bannerId);
+        let banner = await Banner.findById(bannerId);
         if (!banner) {
             return new NextResponse("Banner not found", { status: 404 });
         }
@@ -32,7 +34,7 @@ export const POST = async (req: NextRequest, { params }: { params: { bannerId: s
         if (!title || !image || !url) {
             return new NextResponse("Banner name, image and url are required", { status: 400 });
         }
-        banner = await Banner.findByIdAndUpdate(params.bannerId, { title, description, image, url }, { new: true });
+        banner = await Banner.findByIdAndUpdate(bannerId, { title, description, image, url }, { new: true });
         await banner.save();
         return NextResponse.json(banner, { status: 200 });
     } catch (error) {
@@ -41,14 +43,15 @@ export const POST = async (req: NextRequest, { params }: { params: { bannerId: s
     }
 }
 
-export const DELETE = async (req: NextRequest, { params }: { params: { bannerId: string } }) => {
+export const DELETE = async (req: NextRequest, context: { params: { bannerId: string } }) => {
     try {
         const { userId } = await auth();
         if (!userId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+        const { bannerId } = context.params;
         await connectToDB();
-        await Banner.findByIdAndDelete(params.bannerId);
+        await Banner.findByIdAndDelete(bannerId);
         return new NextResponse("Banner is deleted", { status: 200 });
     } catch (error) {
         console.log("[bannerId_DELETE]", error);
