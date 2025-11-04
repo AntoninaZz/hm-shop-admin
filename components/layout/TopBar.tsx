74cab4697b9ae6ba6ca6ae63488785933fdf4962
AntoninaZz <zdanton02@gmail.com>
2025-11-04 14:12:32 +0200

    close menu when clicked outside or clicked on one of menu links

--- components/layout/TopBar.tsx
+++ components/layout/TopBar.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { UserButton } from '@clerk/nextjs';
@@ -11,6 +11,24 @@ import { navLinks } from '@/lib/constants';
 const TopBar = () => {
   const [dropdownMenu, setDropdownMenu] = useState(false);
   const path = usePathname();
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setDropdownMenu(false);
+      }
+    };
+    if (dropdownMenu) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownMenu]);
+
   return (
     <div className='sticky top-0 z-2 w-full flex justify-between items-center px-8 py-4 bg-[var(--color-milk)] lg:hidden'>
       <Link href={`${process.env.NEXT_PUBLIC_ECOMMERCE_STORE_URL}/`} target='_blank'>
@@ -24,13 +42,13 @@ const TopBar = () => {
         ))}
       </div>
       <div className='flex gap-4 items-center'>
-        <div className='relative'>
+        <div ref={menuRef} className='relative'>
           <MenuIcon className='cursor-pointer md:hidden' onClick={() => { setDropdownMenu(!dropdownMenu); }} />
           {
             dropdownMenu && (
               <div className='absolute top-10 right-0 flex flex-col gap-8 py-3 bg-white shadow-xl rounded-md md:hidden'>
                 {navLinks.map((link, i) => (
-                  <Link key={i} href={link.url} className={`flex gap-4 px-5 py-2 hover:text-[var(--color-muted-green)] transition-all ease-in-out duration-200 ${path === link.url ? 'border-l-2' : ''}`}>
+                  <Link key={i} href={link.url} onClick={() => setDropdownMenu(false)} className={`flex gap-4 px-5 py-2 hover:text-[var(--color-muted-green)] transition-all ease-in-out duration-200 ${path === link.url ? 'border-l-2' : ''}`}>
                     {link.icon}
                     <p>{link.label}</p>
                   </Link>
